fb2dd34302da4c28bead2ee63dd7e4f4505908fb
Rob Foulkrod <89028299+rob-foulkrod@users.noreply.github.com>
2025-01-16 10:42:48 -0500

    Further remove analytics

--- infra/hooks/postup.ps1
+++ infra/hooks/postup.ps1
@@ -1,9 +1,9 @@
-
+# tested collection. Not invoked.
 if($env:TELEMETRY_ENABLED -eq 'true') {
     Write-Host "Telemetry is enabled. Thank you for supporting the project."
     try {
         $client = [Microsoft.ApplicationInsights.TelemetryClient]::new()
-        $client.InstrumentationKey = 'fc040a3a-7a9a-4cab-abd8-57a6bd1e282a'
+        $client.InstrumentationKey = ''
         $client.Context.Cloud.RoleName = 'rob-foulkrod/TollBooth'
         $client.TrackEvent('DeploymentComplete')
         $client.Flush()
